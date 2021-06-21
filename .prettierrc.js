46a31fee511cac27e25d73a05f5dd8abba0a2ae4
Ivo <ivoprovensi1@gmail.com>
2021-06-20 21:40:46 -0400

    inicio: primeira aula NLW Together, conexão com firebase

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  tabWidth: 2,
+  printWidth: 80,
+  endOfLine: 'auto',
+  arrowParens: 'always',
+  trailingComma: 'none',
+  semi: true,
+  useTabs: true,
+  singleQuote: false,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  parser: "typescript"
+};
